4604e8094a3b2f45d6573430dfc99023aae5affc
mmoke.nape <mmoke.nape@entelect.co.za>
2025-06-06 00:23:46 +0200

    revert for successful deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,18 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: "/mmoke-nape-portfolio-crafted",
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
+  base: "/mmoke-nape-portfolio-crafted/",
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 }));
