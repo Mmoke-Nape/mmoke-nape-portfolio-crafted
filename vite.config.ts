9ecdeb1946e988d71e87a7dd41d7d8c1ccc0a1e2
mmoke.nape <mmoke.nape@entelect.co.za>
2025-06-06 23:51:40 +0200

    path access

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
-  base: "/mmoke-nape-portfolio-crafted/",
+  base: mode === "production" ? "/mmoke-nape-portfolio-crafted/" : "/",
 
   resolve: {
     alias: {
